0e23dd89461378669502e36e40a46215a974e000
MdYeasinSamadArnob <yeasinsamadarnob@gmail.com>
2023-10-26 12:22:21 +0600

    Initial commit from Create Next App

---
+++ components/FolderNode.tsx
@@ -0,0 +1,79 @@
+import { FcFolder, FcOpenedFolder, FcPlus } from "react-icons/fc";
+import { TreeNode } from "../App";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+const FolderNode = ({
+  folderData,
+  folderOpenIconHandler,
+  folderCreateCloseHandler,
+}: {
+  folderData: TreeNode;
+  folderOpenIconHandler: (node: TreeNode) => void;
+  folderCreateCloseHandler: (node: TreeNode, type: string) => void;
+}) => {
+  return (
+    <>
+      <div
+        className="childRoots"
+        onClick={() => folderOpenIconHandler(folderData)}
+      >
+        <div className="innrerContainer">
+          <p>{folderData?.isOpen ? <FcOpenedFolder /> : <FcFolder />}</p>
+          <h4>{folderData?.name}</h4>
+        </div>
+        <div className="iconContainer">
+          {folderData.id !== "6534118b68afc9c9ec415ecc" && (
+            <span
+              onClick={(e) => {
+                e.stopPropagation();
+                folderCreateCloseHandler(folderData, "close");
+              }}
+            >
+              <AiOutlineCloseCircle />
+            </span>
+          )}
+          <span
+            onClick={(e) => {
+              e.stopPropagation();
+              folderCreateCloseHandler(folderData, "add");
+            }}
+          >
+            <FcPlus />
+          </span>
+        </div>
+      </div>
+      {/* 
+      {folderData.isOpen && folderData?.child.length > 0 && (
+        <div style={{ marginLeft: "20px" }}>
+          {folderData?.child.map((childNode, index) => (
+            <FolderNode
+              key={index}
+              folderData={childNode}
+              folderOpenIconHandler={folderOpenIconHandler}
+              folderCreateCloseHandler={folderCreateCloseHandler}
+            />
+          ))}
+        </div>
+      )} */}
+      {folderData.isOpen &&
+        (folderData?.child.length > 0 ? (
+          <div style={{ marginLeft: "20px" }}>
+            {folderData?.child.map((childNode, index) => (
+              <FolderNode
+                key={index}
+                folderData={childNode}
+                folderOpenIconHandler={folderOpenIconHandler}
+                folderCreateCloseHandler={folderCreateCloseHandler}
+              />
+            ))}
+          </div>
+        ) : (
+          <div style={{ marginLeft: "20px" }} className="childRoots">
+            <p style={{ fontSize: "10px", opacity: ".5" }}>Empty</p>
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default FolderNode;
---
+++ components/helper.ts
@@ -0,0 +1,136 @@
+import { TreeNode } from "../App";
+
+export const FolderDataSet: TreeNode = {
+  name: "Root",
+  isOpen: false,
+  id: "1",
+  child: [
+    {
+      name: "Folder 1",
+      isOpen: false,
+      id: "2",
+      child: [
+        {
+          name: "Folder 1.1",
+          isOpen: false,
+          id: "4",
+          child: [],
+        },
+      ],
+    },
+    {
+      name: "Folder 2",
+      isOpen: false,
+      id: "3",
+      child: [
+        {
+          name: "Folder 2.1",
+          isOpen: false,
+          id: "5",
+          child: [],
+        },
+        {
+          name: "Folder 2.2",
+          isOpen: false,
+          id: "6",
+          child: [
+            {
+              name: "Folder 2.2.1",
+              isOpen: false,
+              id: "7",
+              child: [
+                {
+                  name: "Folder 2.2.1.1",
+                  isOpen: false,
+                  id: "8",
+                  child: [],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export const toggleFolder = (
+  id: string,
+  folderData: TreeNode,
+  setFolderData: (node: TreeNode) => void
+) => {
+  const updateNodeToggle = (child: TreeNode) => {
+    if (child.id === id) {
+      if (child.isOpen && child.child.length > 0) {
+        closeNextAllChildNode(child);
+      } else {
+        child.isOpen = !child.isOpen;
+      }
+    } else if (child.child) {
+      child.child.forEach(updateNodeToggle);
+    }
+  };
+  const updatedData = { ...folderData };
+  updateNodeToggle(updatedData);
+  setFolderData(updatedData);
+};
+
+const closeNextAllChildNode = (child: TreeNode): void => {
+  child.isOpen = false;
+  if (child.child.length > 0) {
+    child.child.forEach(closeNextAllChildNode);
+  }
+};
+
+export const deleteFolder = (
+  id: string,
+  folderData: TreeNode,
+  setFolderData: (node: TreeNode) => void,
+  cb: () => void
+) => {
+  const updateNode = (child: TreeNode) => {
+    const index = child?.child.findIndex((i) => i.id === id);
+    if (index === -1) {
+      child.child.forEach(updateNode);
+    } else {
+      child?.child.splice(index, 1);
+    }
+  };
+  const updatedData = { ...folderData };
+  updateNode(updatedData);
+  setFolderData(updatedData);
+  cb?.();
+};
+
+export const AddFolder = (
+  id: string,
+  folderName: string,
+  newFolderId: string,
+  folderData: TreeNode,
+  setFolderData: (node: TreeNode) => void,
+  cb: () => void
+) => {
+  const newObject: TreeNode = {
+    name: folderName,
+    isOpen: false,
+    id: newFolderId,
+    child: [] as TreeNode[],
+  };
+
+  if (folderData.name === "Root" && folderData.id === id) {
+    (folderData.child as TreeNode[]).push(newObject);
+  } else {
+    const updateNode = (child: TreeNode) => {
+      const index = child?.child.findIndex((i) => i.id === id);
+      if (index === -1) {
+        child.child.forEach(updateNode);
+      } else {
+        (child?.child[index].child as TreeNode[]).push(newObject);
+      }
+    };
+    const updatedData = { ...folderData };
+    updateNode(updatedData);
+    setFolderData(updatedData);
+  }
+  cb?.();
+};
